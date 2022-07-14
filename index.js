eb992b447b6a6e3737fc75c9b0246b2f1d4c35ea
Parker Fisher <pfishe13@kent.edu>
2022-07-14 10:21:14 -0400

    Add winner and x and o marks on board

--- index.js
+++ index.js
@@ -3,31 +3,86 @@
 const Gameboard = (() => {
     let gameArray = [];
     let playCount = 0;
+    let winnerPlayer;
 
     const setUpEvents = () => {
         console.log(`Game Has Started with ${ player1.getName() } and ${ player2.getName() }`);
         const gameSlots = document.querySelectorAll(".game-slot");
         gameSlots.forEach(slot => { 
-            slot.addEventListener('click', addToArray);
+            slot.addEventListener('click', gameTurn);
         })
     }
 
-    const addToArray = (e) => {
-        console.log(`Index ${e.srcElement.id}`);
-        const index = e.srcElement.id;
-        if (playCount % 2 === 0)
+    const addToArray = (index) => {
+        const gameboardSlot = document.getElementById(index);
+        if (playCount % 2 === 0) {
             gameArray[index] = player1.getName();
-        else {
+            gameboardSlot.innerHTML = `<img class="mark" src = "/circle-outline-svgrepo-com.svg" alt="O"/>`
+        } else {
             gameArray[index] = player2.getName();
+            gameboardSlot.innerHTML = `<img class="mark" src = "/x-svgrepo-com.svg" alt="X"/>`
         }
+    }
+
+    const gameTurn = (e) => {
+        //console.log(`Index ${e.srcElement.id}`);
+        const index = e.srcElement.id;
+
+        addToArray(index);
+
         playCount++;
 
         if (playCount >= 9) 
             endGame();
-        
-        console.log(`Play Count: ${playCount}`);
-        console.log(gameArray);
+
+        if (playCount >= 3) {
+            checkForWinner();
+        }
     };
+
+    const findWinner = (stringName) => {
+        console.log(`Winner: ${stringName}`);
+        if (player1.getName() === stringName) return player1;
+        if (player2.getName() === stringName) return player2;
+    }
+
+    const checkForWinner = () => {
+        if (gameArray[0] !== undefined & (gameArray[0] === gameArray[1]) & (gameArray[1] === gameArray[2])) {
+            console.log(`Winner in first row`);
+            winnerPlayer = findWinner(gameArray[0]);
+        }
+        if (gameArray[3] !== undefined & gameArray[3] === gameArray[4] & gameArray[4] === gameArray[5]) {
+            console.log(`Winner in second row`);
+            winnerPlayer = findWinner(gameArray[3]);
+        }
+        if (gameArray[6] !== undefined & gameArray[6] === gameArray[7] & gameArray[7] === gameArray[8]) {
+            console.log(`Winner in third row`);
+            winnerPlayer = findWinner(gameArray[6]);
+        }
+        if (gameArray[0] !== undefined & gameArray[0] === gameArray[3] & gameArray[3] === gameArray[6]) {
+            console.log(`Winner in first column`);
+            winnerPlayer = findWinner(gameArray[0]);
+        }
+        if (gameArray[1] !== undefined & gameArray[1] === gameArray[4] & gameArray[4] === gameArray[7]) {
+            console.log(`Winner in second column`);
+            winnerPlayer = findWinner(gameArray[1]);
+        }
+        if (gameArray[2] !== undefined & gameArray[2] === gameArray[5] & gameArray[5] === gameArray[8]) {
+            console.log(`Winner in third column`);
+            winnerPlayer = findWinner(gameArray[2]);
+        }
+        if (gameArray[0] !== undefined & gameArray[0] === gameArray[4] & gameArray[4] === gameArray[8]) {
+            console.log(`Winner in diagonal`);
+            winnerPlayer = findWinner(gameArray[0]);
+        }
+        if (gameArray[2] !== undefined & gameArray[2] === gameArray[4] & gameArray[4] === gameArray[6]) {
+            console.log(`Winner in diagonal`);
+            winnerPlayer = findWinner(gameArray[2]);
+        }
+
+        if (winnerPlayer) endGame();
+
+    }
 
     const outputArray = () => {
         for(let i = 0; i < gameArray.length; i++) {
@@ -41,7 +96,7 @@ const Gameboard = (() => {
         board.style.display = "none";
     }
 
-    return { setUpEvents, addToArray, outputArray };
+    return { setUpEvents, outputArray };
 })();
 
 // Display Controller Module Object 
@@ -59,7 +114,7 @@ const GameController = (() => {
                 player1.changeName(playerOneName);
             }
             if (playerTwoName !== "") {
-                player1.changeName(playerTwoName);
+                player2.changeName(playerTwoName);
             }
 
             Gameboard.setUpEvents();
